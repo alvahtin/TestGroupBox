028fd1d37088417a7fe135c0f8d6244c0302b107
Вахтин Алексей Александрович <50268307+alvahtin@users.noreply.github.com>
2024-07-27 10:44:07 +0300

    Add files via upload

---
+++ TestAvalonia/Controls/GroupBox.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls.Primitives;
+
+namespace FoundationLibrary.Controls;
+
+/// <summary>
+/// Эдемент управления с рамкой и заголовком
+/// </summary>
+public partial class GroupBox : HeaderedContentControl
+{
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    public GroupBox()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TestAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TestAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
